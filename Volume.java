8666a9a660f96650bcb312340b65381b0ab20556
Mayank795 <mayankspal555@gmail.com>
2025-01-07 22:53:59 +0530

    find the volume program

---
+++ Volume.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class C_to_F{
+ public static void main(String[] args){
+    
+   Scanner scanner = new Scanner(System.in);
+   int r = scanner.nextInt();
+   int h = scanner.nextInt();
+    int v = Math.PI * Math.pow(r, 2) * h;
+System.out.println(v);
+
+  }
+}
