16ebd0df46c30dcc51365fc97ac72abbff5eb2e2
Mayank795 <mayankspal555@gmail.com>
2025-01-07 22:48:51 +0530

    C to F program

---
+++ C_to_F.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class C_to_F{
+ public static void main(String[] args){
+    
+   Scanner scanner = new Scanner(System.in);
+   int a = scanner.nextInt();
+   int b = (a* (9/5))+32;
+ 
+System.out.println(b);
+
+  }
+}
