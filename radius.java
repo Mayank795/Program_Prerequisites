bef1969d2bf3e5290e36ac6e603069d5515de1a5
Mayank795 <mayankspal555@gmail.com>
2025-01-07 22:51:37 +0530

    radius of a circle

---
+++ radius.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class C_to_F{
+ public static void main(String[] args){
+    
+   Scanner scanner = new Scanner(System.in);
+   int a = scanner.nextInt();
+   int b = Math.sqrt(a / Math.PI);
+;
+ 
+System.out.println(b);
+
+  }
+}
