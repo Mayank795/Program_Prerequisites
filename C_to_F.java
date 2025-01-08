52ab0f93ec9322074f1907d3e43403eb386f53d3
Mayank795 <mayankspal555@gmail.com>
2025-01-08 11:34:03 +0530

    update

--- C_to_F.java
+++ C_to_F.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 public class C_to_F{
  public static void main(String[] args){
-    
+    System.out.println("Celsius to Fehrenheit");
    Scanner scanner = new Scanner(System.in);
-   int a = scanner.nextInt();
-   int b = (a* (9/5))+32;
+   System.out.println("enter celsius");
+   float cel = scanner.nextInt();
+   float  fah= (cel* (9f/5))+32;
  
-System.out.println(b);
+System.out.println(fah);
 
   }
 }
