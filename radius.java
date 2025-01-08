cfc9773bc091b66f0d8ba8279c7c11d0ee5f8586
Mayank795 <mayankspal555@gmail.com>
2025-01-08 11:39:35 +0530

    update

--- radius.java
+++ radius.java
@@ -1,14 +1,16 @@
 import java.util.*;
 
-public class C_to_F{
+public class radius{
  public static void main(String[] args){
-    
+    System.out.println("Area of a circle");
    Scanner scanner = new Scanner(System.in);
-   int a = scanner.nextInt();
-   int b = Math.sqrt(a / Math.PI);
-;
+System.out.println("enter radius");
+
+   int rad = scanner.nextInt();
+   float area = (22f/7)*rad*rad;
+
  
-System.out.println(b);
+System.out.println(area);
 
   }
 }
