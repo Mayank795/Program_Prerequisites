a9894990846a601550772324c83b796f998524e8
Mayank795 <mayankspal555@gmail.com>
2025-01-08 11:32:42 +0530

    program

---
+++ average.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class average{
+ public static void main(String[] args){
+    System.out.println("average of three numbers");
+
+   Scanner scanner = new Scanner(System.in);
+   System.out.println("enter number");
+
+   int a = scanner.nextInt();
+System.out.println("enter number");
+
+   int b = scanner.nextInt();
+System.out.println("enter number");
+
+   int c = scanner.nextInt();
+
+    
+System.out.println("average of three numbers"+ " "+(a+b+c)/3);
+
+  }
+}
