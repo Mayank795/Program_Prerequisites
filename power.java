e2625ea0840f123577af238250bb25e04910fb7c
Mayank795 <mayankspal555@gmail.com>
2025-01-08 11:57:51 +0530

    program

---
+++ power.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class power{
+ public static void main(String[] args){
+          Scanner scanner = new Scanner(System.in);
+   System.out.println("enter base");
+
+  double b =scanner.nextDouble(); 
+
+   System.out.println("enter exponent");
+
+  double e =scanner.nextDouble();  
+
+    System.out.println("result "+ Math.pow(b,e));
+	
+	}
+	}
