584da09db75e7bd0bc5dccd1330664bb0374fdf5
Mayank795 <mayankspal555@gmail.com>
2025-01-08 11:41:36 +0530

    update

--- Volume.java
+++ Volume.java
@@ -1,13 +1,18 @@
 import java.util.*;
 
-public class C_to_F{
+public class Volume{
  public static void main(String[] args){
-    
+    System.out.println("Volume of Cylinder");
+
    Scanner scanner = new Scanner(System.in);
+   System.out.println("enter radius");
+
    int r = scanner.nextInt();
+System.out.println("enter hieght");
+
    int h = scanner.nextInt();
-    int v = Math.PI * Math.pow(r, 2) * h;
-System.out.println(v);
+    float v =(22f/7)*r*r*h;
+System.out.println("volume of Cylinder"+ " "+v);
 
   }
 }
