8f291345ba437cdb9dc5a688da593108ca17149c
Mayank795 <mayankspal555@gmail.com>
2025-01-08 11:40:41 +0530

    updtae

--- sum.java
+++ sum.java
@@ -4,10 +4,14 @@ public class sum{
  public static void main(String[] args){
     
    Scanner scanner = new Scanner(System.in);
+System.out.println("enter value");
+
    int a = scanner.nextInt();
+System.out.println("enter value");
+
    int b = scanner.nextInt();
  
-System.out.println(a+b);
+System.out.println("sum of those values"+"  " (a+b));
 
   }
 }
